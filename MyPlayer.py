565f6a1e5650c3e0067befd2b5dee5c621b1fd03
Andrewindykov <55688672+Andrewindykov@users.noreply.github.com>
2022-10-14 14:31:35 +0300

    ходит но не стреляет

--- MyPlayer.py
+++ MyPlayer.py
@@ -2,14 +2,15 @@ import pygame
 from constants import *
 
 class Player():
-    def __init__(self):
+    def __init__(self, name: str):
         self.state = ALIVE
         self.x = START_X
         self.y = START_Y
         self.direction = RIGHT
-        self.name = 'Lancelot'
+        self.name = name
         self.hp = MAX_HP
         self.mp = MAX_MP
+        self.mooving = [0, 0, 0, 0]
         #self.image_pack = ['data/archerr.png','data/archerd.png','data/archerl.png','data/archeru.png']
         # self.images = []
         # for image in self.image_pack:
@@ -23,19 +24,34 @@ class Player():
         i = []
         i.append(temp.subsurface(240, 186, 60, 88))
         i.append(temp.subsurface(0, 0, 60, 88))
-        i.append(temp.subsurface(0, 364, 60, 88))
+        i.append(temp.subsurface(0, 363, 59, 87))
         i.append(temp.subsurface(300, 0, 60, 88))
         i.append(temp.subsurface(60, 0, 60, 88))
         self.image = i
 
-        self.mooving[0, 0, 0, 0]
+
 
     def moove(self):
-        pass
+
+
+        if self.mooving[RIGHT] == 1:
+            self.x+=PLAYER_SPEED
+        if self.mooving[DOWN]:
+            self.y+=PLAYER_SPEED
+        if self.mooving[LEFT]:
+            self.x-=PLAYER_SPEED
+        if self.mooving[UP]:
+            self.y-=PLAYER_SPEED
+
+        if self.x <= 0: self.x = 0
+        if self.y <= 0: self.y = 0
+        if self.x >= SCREEN_WIDTH-35: self.x = SCREEN_WIDTH-35
+        if self.y >= SCREEN_HEIGHT-75: self.y = SCREEN_HEIGHT-75
+
 
     def render(self, screen):
        # screen.blit(self.images[self.direction][self.state], (self.x, self.y))
-       screen.blit(self.images[self.direction], (self.x, self.y))
+       screen.blit(self.image[self.direction], (self.x, self.y))
     def render_ui(self, screen):
         pass
 
--- mymain.py
+++ mymain.py
@@ -1,25 +1,72 @@
 import pygame
+from pygame import *
 from constants import *
+from MyPlayer import Player
 
 class Main():
     def __init__(self, screen):
         self.screen = screen
+        self.player = Player('Grut')
         self.background = pygame.image.load('data/background.jpg')
         self.running = True
         self.main_loop()
 
     def handle_events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_RIGHT:
+                    self.player.direction = RIGHT
+                    self.player.mooving = [1,0,0,0]
+                if event.key == K_DOWN:
+                    self.player.direction = DOWN
+                    self.player.mooving = [0,1,0,0]
+                if event.key == K_LEFT:
+                    self.player.direction = LEFT
+                    self.player.mooving = [0,0,1,0]
+                if event.key == K_UP:
+                    self.player.direction = UP
+                    self.player.mooving = [0,0,0,1]
+
+                if event.key == K_SPACE:
+                    if self.player.state != DEAD:
+                        self.player.state = DEAD
+                    else:
+                        self.player.state = ALIVE
+
+                if event.key == K_z:
+                    if self.player.state != SHOOT:
+                        self.player.state = SHOOT
+                    else:
+                        self.player.state = ALIVE
+
+
+
+            elif event.type == KEYUP:
+                if event.key == K_RIGHT:
+                    self.player.mooving[RIGHT] = 0
+                if event.key == K_DOWN:
+                    self.player.mooving[DOWN] = 0
+                if event.key == K_LEFT:
+                    self.player.mooving[LEFT] = 0
+                if event.key == K_UP:
+                    self.player.mooving[UP] = 0
+
 
     def render(self):
 
         self.screen.blit(self.background,(0,0))
+        self.player.render(screen)
         pygame.display.flip()
 
     def main_loop(self):
         while self.running == True:
+            if self.player.state != DEAD:
+                self.player.moove()
             self.render()
-# проверка связи
+            self.handle_events()
+
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
