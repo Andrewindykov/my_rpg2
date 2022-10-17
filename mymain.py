b1315ecdc3ee5af84b6fa6f7c011ed7168df8159
Andrewindykov <55688672+Andrewindykov@users.noreply.github.com>
2022-10-17 12:10:51 +0300

    стркеляет. Конец 5го урока. картинки стрел тоже есть

--- mymain.py
+++ mymain.py
@@ -1,45 +1,63 @@
 import pygame
-from pygame import *
+import time
+from pygame.locals import *
 from constants import *
 from MyPlayer import Player
+from Projective import *
 
 class Main():
     def __init__(self, screen):
         self.screen = screen
         self.player = Player('Grut')
+        self.projective = []
         self.background = pygame.image.load('data/background.jpg')
         self.running = True
+        self.count = 0
+
         self.main_loop()
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+
+            elif event.type == USEREVENT+1:
+                self.player.tick()
             elif event.type == KEYDOWN:
                 if event.key == K_RIGHT:
-                    self.player.direction = RIGHT
                     self.player.mooving = [1,0,0,0]
                 if event.key == K_DOWN:
-                    self.player.direction = DOWN
-                    self.player.mooving = [0,1,0,0]
+                     self.player.mooving = [0,1,0,0]
                 if event.key == K_LEFT:
-                    self.player.direction = LEFT
-                    self.player.mooving = [0,0,1,0]
+                       self.player.mooving = [0,0,1,0]
                 if event.key == K_UP:
-                    self.player.direction = UP
                     self.player.mooving = [0,0,0,1]
 
                 if event.key == K_SPACE:
+                    self.player.hp-=5
                     if self.player.state != DEAD:
                         self.player.state = DEAD
+                        self.player.direction = DIE
                     else:
                         self.player.state = ALIVE
+                        self.player.direction = UP
 
                 if event.key == K_z:
-                    if self.player.state != SHOOT:
+                    if self.player.mp >= SKILL1_COST and self.player.state != SHOOT and self.player.state != DEAD:
+                        self.player.mp -= SKILL1_COST
                         self.player.state = SHOOT
-                    else:
-                        self.player.state = ALIVE
+                        self.player.spell_casted = pygame.time.get_ticks()
+
+
+                        if self.player.direction == RIGHT:
+                            self.projective.append(Arrow(self.player.x+12, self.player.y, self.player.direction))
+                        elif self.player.direction == DOWN:
+                            self.projective.append(Arrow(self.player.x, self.player.y+12, self.player.direction))
+                        elif self.player.direction == LEFT:
+                            self.projective.append(Arrow(self.player.x-12, self.player.y, self.player.direction))
+                        else:
+                            self.projective.append(Arrow(self.player.x, self.player.y-12, self.player.direction))
+
 
 
 
@@ -58,16 +76,24 @@ class Main():
 
         self.screen.blit(self.background,(0,0))
         self.player.render(screen)
+        self.player.render_ui(screen)
+        for i in self.projective:
+            i.render(screen)
         pygame.display.flip()
 
     def main_loop(self):
         while self.running == True:
             if self.player.state != DEAD:
                 self.player.moove()
+            for i in self.projective:
+                i.moove()
             self.render()
             self.handle_events()
+            #self.pygame.time.Clock.tick(33)
+            clock.tick(50)
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+clock = pygame.time.Clock()
+pygame.time.set_timer(USEREVENT+1, 100)
 game = Main(screen)
